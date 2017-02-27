f122a04047897a70c6290c60201a59df003ad516
Kate Osborn <kosborn@colorado.edu>
2017-02-27 01:21:22 -0700

    refactored the code to make it more extensible, changed the sprite source

--- js/captain_cool.js
+++ js/captain_cool.js
@@ -1,90 +1,154 @@
-function startGame() {
-  // declare all the big variables here
-  myGameArea.start();
+var KEYS = [];
+
+class Character {
+  constructor(context, width, height, image, speed) {
+    this.context = context;
+    this.width = width;
+    this.height = height;
+    this.image =  new Image();
+    this.image.src = image;
+    this.x = 0; //position x
+    this.y = 0; //position y
+    this.cutX = 0;
+    this.cutY = 0;
+    this.direction = DIRECTIONS.RIGHT;
+    this.animate = false;
+    this.speed = speed;
+  }
+
+  draw() {
+    this.move();
+    this.context.drawImage(this.image, (this.cutX * this.width),
+                          (this.height * this.cutY), this.width, this.height,
+                          this.x, this.y,
+                          this.width, this.height);
+  }
+
+  move() {}  
 }
 
-var myGameArea = {
-  // Create the canvas
-  canvas : document.createElement("canvas"),
-  start : function() {
-    // Canvas width and height
-    this.canvas.width = 960;
-    this.canvas.height = 640;
-		this.canvas.style.backgroundColor = 'white'
-    // Game will be 2d
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    window.addEventListener('keydown', function (e) { // Listen for and catch keyboard input
-		myGameArea.move = true; //Sprite movement boolean
-		myGameArea.keys=(myGameArea.keys || []); //Boolean array of which keys are down
-		myGameArea.keys[e.keyCode] = true;
-	})
-	window.addEventListener('keyup', function (e) {
-		myGameArea.move = false;
-		myGameArea.keys[e.keyCode] = false; 
-	})
-    // Start at frame zero
-    this.frameNo = 0;
-	draw();
+class PlayerCharacter extends Character {
+  constructor(context, width, height, image, speed) {
+    super(context, width, height, image, speed);
+    this.count = 0;
+  }
+
+  move() {
+   		if (KEYS[37])
+      {
+        this.animate = true;
+        this.x -= this.speed;
+        this.direction = DIRECTIONS.LEFT;
+      } // If left key pressed go left
+ 		  else if (KEYS[39])
+      {
+        this.animate = true;
+        this.x += this.speed;
+        this.direction = DIRECTIONS.RIGHT;
+      } // Right key
+      else if (KEYS[38])
+      {
+        this.animate = true;
+        this.y -= this.speed;
+        this.direction = DIRECTIONS.DOWN;
+      } // Down key
+      else if (KEYS[40]) {
+        this.animate = true;
+        this.y += this.speed;
+        this.direction = DIRECTIONS.UP;
+      } // Up key
+      else {
+        this.animate = false;
+      }
+      this.setAnimationFrame();
+  }
+
+
+  setAnimationFrame() {
+        if(!this.animate)
+          return;
+        //set yImage based on direction for most sprites
+        this.cutY = this.direction;
+        if(this.count % 15 == 0) { // Every 15 movement frames (frame group) cycle through sprites
+          if (this.count == 0){ // Begin cycle at sprite 2
+            this.cutX = 0;
+          }
+          else if (this.count <= 30){ // Go to next sprite for 3 frame groups
+            this.cutX++;
+          }
+          else if (this.count <= 60){ // Go to previous sprite for 2 frame groups
+            this.cutX--;
+          }
+          else { // Reset cycle
+            this.count = -1; // Increments up to 0 before next call
+          }
+        }
+        this.count++;
+    }
+
+}
+
+var DIRECTIONS = {
+  UP : 0,
+  LEFT : 1,
+  RIGHT : 2,
+  DOWN : 3
+}
+
+class InputHandler {
+  constructor() {
+    window.addEventListener('keydown', function (e) {
+ 		      KEYS =(KEYS || []); //Boolean array of which keys are down
+ 		      KEYS[e.keyCode] = true;
+ 	  })
+ 	  window.addEventListener('keyup', function (e) {
+ 		    KEYS[e.keyCode] = false;
+ 	    })
   }
 }
 
+
+
+class Game {
+  constructor(canvas)  {
+    this.inputHandler = new InputHandler()
+    this.drawables = [];
+    this.canvas = canvas;
+    this.context = this.canvas.getContext('2d');
+  }
+
+  addDrawable(d) {
+    this.drawables.push(d);
+  }
+
+  removeDrawable(d) {
+    this.drawables.pop(d);
+  }
+
+  start() {
+    this.canvas.width = 960;
+    this.canvas.height = 640;
+    this.canvas.style.backgroundColor = 'white'
+    // Game will be 2d
+    draw();
+  }
+}
+
+
+canvas = document.getElementById('canvas');
+var gameInstance = new Game(canvas)
+
+//can't live in the class so we gave to do this hacky crap
 function draw() {
-	var ctx = myGameArea.context;
-	sprite = createSprite({
-		context: ctx,
-		width: 190,
-		height: 325,
-		image: spriteImage
-	});
-	
-	ctx.clearRect(0, 0, 960, 640);
-
-	ctx.drawImage(sprite.image, (50 + 200 * a), 420,
-								sprite.width, sprite.height,
-								x, y, (sprite.width), sprite.height);
-	if (myGameArea.move){ // If one or more keys are pressed
-		if (myGameArea.keys[37]) {x -= 4;} // If left key pressed go left
-		if (myGameArea.keys[39]) {x += 4;} // Right key
-		if (myGameArea.keys[38]) {y -= 4;} // Down key
-		if (myGameArea.keys[40]) {y += 4;} // Up key
-		setSprite(); // Update sprite
-	}
-	window.requestAnimationFrame(draw);
+    gameInstance.context.clearRect(0, 0, 960, 640);
+    gameInstance.drawables.forEach(function(d) {
+      d.draw(0,0,0,0)
+    });
+    window.requestAnimationFrame(draw);
 }
 
-//factory that creates a sprite and takes a JSON called options
-function createSprite (options) {
-	var newSprite = {};
-	newSprite.context = options.context;
-	newSprite.width = options.width;
-	newSprite.height = options.height;
-	newSprite.image = options.image;
-	return newSprite;
-}
 
-var spriteImage = new Image();
-spriteImage.src = "static/images/spritesheet.jpg";
-	
-var x = 0; // Sprite's horizontal position
-var y = 0; // Sprite's veritcal position
-var a = 2; // Current sprite
-var count = 0; // Number of movement frames
-function setSprite() {
-	if(count % 15 == 0) { // Every 15 movement frames (frame group) cycle through sprites
-		if (count == 0){ // Begin cycle at sprite 2
-			a = 2;
-		}
-		else if (count <= 45){ // Go to next sprite for 3 frame groups
-			a++;
-		}
-		else if (count <= 75){ // Go to previous sprite for 2 frame groups
-			a--;
-		}
-		else { // Reset cycle
-			count = -1; // Increments up to 0 before next call
-		}
-		console.log(a);
-	}
-	count++;
-}
+var character = new PlayerCharacter(gameInstance.context, 32, 32, 'img/better_sprite.png', 2)
+gameInstance.addDrawable(character);
+//now that draw is defined we can start the game
+gameInstance.start();
