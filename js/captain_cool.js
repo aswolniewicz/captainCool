447277b2d528ee1397fc61894ff6131c284653ee
fram6578 <fram6578@colorado.edu>
2017-03-06 02:19:10 -0700

    Improved the Character.move() and Game.draw() methods. Spilt js into multiple files

--- js/captain_cool.js
+++ js/captain_cool.js
@@ -1,184 +1,10 @@
-//global array for all pressed keys
-var KEYS = [];
-
-//base drawable class, anything drawn on the screen inherits this.
-class Drawable {
-  constructor(context, width, height, x, y) {
-    this.context = context;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-  }
-  //draw method to overload
-  draw() { }
-}
-
-class MessageArea extends Drawable {
-  constructor(context, width, height, x, y, color) {
-    super(context, width, height, x, y);
-    //can't access this in constructor until we call super
-    this.color = color; //color is a string (think css)
-  }
-
-  draw() {
-    this.context.fillStyle = this.color;
-    this.context.fillRect(this.x, this.y, this.width, this.height);
-  }
-}
-
-//any character that moves extends this class
-class Character extends Drawable {
-  constructor(context, width, height, image, speed, x, y) {
-    //super calls the base class constructor
-    //super.methodname() calls that method from the base class
-    super(context, width, height, x, y)
-    this.image =  new Image();
-    this.image.src = image;
-    this.cutX = 0;
-    this.cutY = 0;
-    this.direction = DIRECTIONS.RIGHT;
-    this.animate = false;
-    this.speed = speed;
-  }
-
-  //draws to canvas context based on the source image and the position
-  draw() {
-    this.move();
-    this.context.drawImage(this.image, (this.cutX * this.width),
-                          (this.height * this.cutY), this.width, this.height,
-                          this.x, this.y,
-                          this.width, this.height);
-  }
-
-  //move must be overridden if we want to do anything
-  move() {}
-}
-
-
-//player character that responds to key input
-class PlayerCharacter extends Character {
-  constructor(context, width, height, image, speed, x, y) {
-    super(context, width, height, image, speed, x, y);
-    this.count = 0;
-  }
-
-  //checks if keys are pressed and if they are, move and animate
-  // uses the WASD keys
-  move() {
-      if (KEYS[65] && KEYS[83]) // Go Left and Down
-      {
-        this.animate = true; // Bool that allows the spire to move
-        this.x -= this.speed; // Move left (x value is decremented)
-        this.y += this.speed; // Move down {why is y incremented?????}
-        this.direction = DIRECTIONS.LEFT; //  use the image of the sprite
-                                            // that is facing left 
-      }
-      else if (KEYS[65] && KEYS[87]) // Go Left and Up
-      {
-        this.animate = true;
-        this.x -= this.speed;
-        this.y -= this.speed;
-        this.direction = DIRECTIONS.LEFT;
-      }
-      else if (KEYS[68] && KEYS[83]) // Go Right and Down
-      {
-        this.animate = true;
-        this.x += this.speed;
-        this.y += this.speed;
-        this.direction = DIRECTIONS.RIGHT;
-      }
-      else if (KEYS[68] && KEYS[87]) // Go Right and Up
-      {
-        this.animate = true;
-        this.x += this.speed;
-        this.y -= this.speed;
-        this.direction = DIRECTIONS.RIGHT;
-      }
-   		else if (KEYS[65]) // Go Left
-      {
-        this.animate = true;
-        this.x -= this.speed;
-        this.direction = DIRECTIONS.LEFT;
-      }
- 		  else if (KEYS[68]) // Go Right 39
-      {
-        this.animate = true;
-        this.x += this.speed;
-        this.direction = DIRECTIONS.RIGHT;
-      }
-      else if (KEYS[87]) // Go Up
-      {
-        this.animate = true;
-        this.y -= this.speed;
-        this.direction = DIRECTIONS.DOWN;
-      }
-      else if (KEYS[83]) // Go Down
-      {
-        this.animate = true;
-        this.y += this.speed;
-        this.direction = DIRECTIONS.UP;
-      }
-      else {
-        this.animate = false;
-      }
-      this.setAnimationFrame();
-  }
-
-  //figure out where on the spritesheet to animate from
-  setAnimationFrame() {
-        if(!this.animate)
-          return;
-        //set yImage based on direction for most sprites
-        this.cutY = this.direction;
-        if(this.count % 15 == 0) { // Every 15 movement frames (frame group) cycle through sprites
-          if (this.count == 0){ // Begin cycle at sprite 2
-            this.cutX = 0;
-          }
-          else if (this.count <= 30){ // Go to next sprite for 3 frame groups
-            this.cutX++;
-          }
-          else if (this.count <= 60){ // Go to previous sprite for 2 frame groups
-            this.cutX--;
-          }
-          else { // Reset cycle
-            this.count = -1; // Increments up to 0 before next call
-          }
-        }
-        this.count++;
-    }
-
-}
-
-//an 'enum' for directions not sure how much milage we will get out
-//of this as it's pretty specific to our only sprite sheet
-var DIRECTIONS = {
-  UP : 0,
-  LEFT : 1,
-  RIGHT : 2,
-  DOWN : 3
-}
-
-//adds the key even listeners when it's constructed
-class InputHandler {
-  constructor() {
-    window.addEventListener('keydown', function (e) {
- 		      KEYS =(KEYS || []); //Boolean array of which keys are down
- 		      KEYS[e.keyCode] = true;
- 	  })
- 	  window.addEventListener('keyup', function (e) {
- 		    KEYS[e.keyCode] = false;
- 	    })
-  }
-}
-
 //the game class, posesses the canvas and calls all of the draw functions
 class Game {
   constructor(canvas)  {
     this.inputHandler = new InputHandler()
     this.drawables = [];
     this.canvas = canvas;
-    this.context = this.canvas.getContext('2d');
+    this.context = this.canvas.getContext('2d'); // Game will be 2d
   }
 
   //anything that's drawable we need to add to this list so that
@@ -191,14 +17,25 @@ class Game {
   removeDrawable(d) {
     this.drawables.pop(d);
   }
-
+  
+  draw(){
+	this.context.clearRect(0, 0, 960, 640);
+    this.drawables.forEach(function(d) {
+      d.draw()
+    });
+    this.detectCollisions();
+    this.resolveGlobalKeys();
+    // weird bullshit that allows requestAnimationFrame inside the class
+    var self=this; 
+    window.requestAnimationFrame(function() {self.draw()});
+  }
+  
   //call the initial global draw function and set some values
   start() {
     this.canvas.width = 960;
     this.canvas.height = 640;
     this.canvas.style.backgroundColor = 'white'
-    // Game will be 2d
-    draw();
+    this.draw();
   }
 
   showMessage() {
@@ -233,21 +70,10 @@ class Game {
 
 }
 
-
 //get the canvas from the html
 canvas = document.getElementById('canvas');
 var gameInstance = new Game(canvas)
 
-//can't live in the class so we gave to do this hacky crap
-function draw() {
-    gameInstance.context.clearRect(0, 0, 960, 640);
-    gameInstance.drawables.forEach(function(d) {
-      d.draw()
-    });
-    gameInstance.detectCollisions();
-    gameInstance.resolveGlobalKeys();
-    window.requestAnimationFrame(draw);
-}
 
 //lets create our character from the sprite sheet
 var character = new PlayerCharacter(gameInstance.context, 32, 32, 'img/better_sprite.png', 2, 0, 0)
