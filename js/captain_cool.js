c7d8a0632158ee45a527e1319f1a72c2eb2f4024
Kate Osborn <kosborn@colorado.edu>
2017-03-06 21:41:01 -0700

    whole bunch of changes see kates changes md file

--- js/captain_cool.js
+++ js/captain_cool.js
@@ -1,181 +1,12 @@
+//allows us to actually catch errors that would otherwise fail silently
+'use strict';
+
 //global array for all pressed keys
 var KEYS = [];
-
-//base drawable class, anything drawn on the screen inherits this.
-class Drawable {
-  constructor(context, width, height, x, y) {
-    this.context = context;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-  }
-  //draw method to overload
-  draw() { }
-}
-
-class MessageArea extends Drawable {
-  constructor(context, width, height, x, y, color) {
-    super(context, width, height, x, y);
-    //can't access this in constructor until we call super
-    this.color = color; //color is a string (think css)
-  }
-
-  draw() {
-    this.context.fillStyle = this.color;
-    this.context.fillRect(this.x, this.y, this.width, this.height);
-  }
-}
-
-//any character that moves extends this class
-class Character extends Drawable {
-  constructor(context, width, height, image, speed, x, y) {
-    //super calls the base class constructor
-    //super.methodname() calls that method from the base class
-    super(context, width, height, x, y)
-    this.image =  new Image();
-    this.image.src = image;
-    this.cutX = 0;
-    this.cutY = 0;
-    this.direction = DIRECTIONS.RIGHT;
-    this.animate = false;
-    this.speed = speed;
-  }
-
-  //draws to canvas context based on the source image and the position
-  draw() {
-    this.move();
-    this.context.drawImage(this.image, (this.cutX * this.width),
-                          (this.height * this.cutY), this.width, this.height,
-                          this.x, this.y,
-                          this.width, this.height);
-  }
-
-  //move must be overridden if we want to do anything
-  move() {}
-}
-
-
-//player character that responds to key input
-class PlayerCharacter extends Character {
-  constructor(context, width, height, image, speed, x, y) {
-    super(context, width, height, image, speed, x, y);
-    this.count = 0;
-  }
-
-  //checks if keys are pressed and if they are, move and animate
-  // uses the WASD keys
-  move() {
-      if (KEYS[65] && KEYS[83]) // Go Left and Down
-      {
-        this.animate = true; // Bool that allows the spire to move
-        this.x -= this.speed; // Move left (x value is decremented)
-        this.y += this.speed; // Move down {why is y incremented?????}
-        this.direction = DIRECTIONS.LEFT; //  use the image of the sprite
-                                            // that is facing left 
-      }
-      else if (KEYS[65] && KEYS[87]) // Go Left and Up
-      {
-        this.animate = true;
-        this.x -= this.speed;
-        this.y -= this.speed;
-        this.direction = DIRECTIONS.LEFT;
-      }
-      else if (KEYS[68] && KEYS[83]) // Go Right and Down
-      {
-        this.animate = true;
-        this.x += this.speed;
-        this.y += this.speed;
-        this.direction = DIRECTIONS.RIGHT;
-      }
-      else if (KEYS[68] && KEYS[87]) // Go Right and Up
-      {
-        this.animate = true;
-        this.x += this.speed;
-        this.y -= this.speed;
-        this.direction = DIRECTIONS.RIGHT;
-      }
-   		else if (KEYS[65]) // Go Left
-      {
-        this.animate = true;
-        this.x -= this.speed;
-        this.direction = DIRECTIONS.LEFT;
-      }
- 		  else if (KEYS[68]) // Go Right 39
-      {
-        this.animate = true;
-        this.x += this.speed;
-        this.direction = DIRECTIONS.RIGHT;
-      }
-      else if (KEYS[87]) // Go Up
-      {
-        this.animate = true;
-        this.y -= this.speed;
-        this.direction = DIRECTIONS.DOWN;
-      }
-      else if (KEYS[83]) // Go Down
-      {
-        this.animate = true;
-        this.y += this.speed;
-        this.direction = DIRECTIONS.UP;
-      }
-      else {
-        this.animate = false;
-      }
-      this.setAnimationFrame();
-  }
-
-  //figure out where on the spritesheet to animate from
-  setAnimationFrame() {
-        if(!this.animate)
-          return;
-        //set yImage based on direction for most sprites
-        this.cutY = this.direction;
-        if(this.count % 15 == 0) { // Every 15 movement frames (frame group) cycle through sprites
-          if (this.count == 0){ // Begin cycle at sprite 2
-            this.cutX = 0;
-          }
-          else if (this.count <= 30){ // Go to next sprite for 3 frame groups
-            this.cutX++;
-          }
-          else if (this.count <= 60){ // Go to previous sprite for 2 frame groups
-            this.cutX--;
-          }
-          else { // Reset cycle
-            this.count = -1; // Increments up to 0 before next call
-          }
-        }
-        this.count++;
-    }
-
-}
-
-//an 'enum' for directions not sure how much milage we will get out
-//of this as it's pretty specific to our only sprite sheet
-var DIRECTIONS = {
-  UP : 0,
-  LEFT : 1,
-  RIGHT : 2,
-  DOWN : 3
-}
-
-//adds the key even listeners when it's constructed
-class InputHandler {
-  constructor() {
-    window.addEventListener('keydown', function (e) {
- 		      KEYS =(KEYS || []); //Boolean array of which keys are down
- 		      KEYS[e.keyCode] = true;
- 	  })
- 	  window.addEventListener('keyup', function (e) {
- 		    KEYS[e.keyCode] = false;
- 	    })
-  }
-}
 
 //the game class, posesses the canvas and calls all of the draw functions
 class Game {
   constructor(canvas)  {
-    this.inputHandler = new InputHandler()
     this.drawables = [];
     this.canvas = canvas;
     this.context = this.canvas.getContext('2d');
@@ -187,10 +18,6 @@ class Game {
     this.drawables.push(d);
   }
 
-  //pretty self explanatory
-  removeDrawable(d) {
-    this.drawables.pop(d);
-  }
 
   //call the initial global draw function and set some values
   start() {
@@ -200,61 +27,53 @@ class Game {
     // Game will be 2d
     draw();
   }
-
-  showMessage() {
-    var messageArea = document.getElementById('message-area');
-    messageArea.innerHTML = 'Test Message: Press enter to dismiss.';
-  }
-
-  hideMessage() {
-    var messageArea = document.getElementById('message-area');
-    messageArea.innerHTML = '';
-  }
-
-  //really great O(n^2) method that needs to be re written
-  //maybe only check every object against objects that moved?
-  detectCollisions() {
-    var self = this;
-    self.drawables.forEach(function(d1) {
-      self.drawables.forEach(function(d2) {
-        if(((d1.x >= d2.x) && (d1.x <= d2.x + d2.width))
-            && ((d1.y >= d2.y) && (d1.y <= d2.y + d2.height)) && !(d1 === d2)) {
-            self.showMessage(); //eventaully need to raise a method that notifies its args of the collision (observer)
-        }
-      });
-    });
-  }
-
-  //keys that impact the entire game
-  resolveGlobalKeys() {
-    if(KEYS[13])
-      this.hideMessage();
-  }
-
 }
 
-
 //get the canvas from the html
-canvas = document.getElementById('canvas');
+var canvas = document.getElementById('canvas');
 var gameInstance = new Game(canvas)
+var collisionResolver = new CollisionResolver();
+var inputHandler = new InputHandler();
 
 //can't live in the class so we gave to do this hacky crap
 function draw() {
     gameInstance.context.clearRect(0, 0, 960, 640);
     gameInstance.drawables.forEach(function(d) {
-      d.draw()
+      d.draw();
     });
-    gameInstance.detectCollisions();
-    gameInstance.resolveGlobalKeys();
+    inputHandler.pollForKeyboardInput();
+    collisionResolver.detectCollisions();
+    if(!isBarrierSolid)
+      barrier3.solid = false;
+    updateLogs();
     window.requestAnimationFrame(draw);
 }
 
 //lets create our character from the sprite sheet
-var character = new PlayerCharacter(gameInstance.context, 32, 32, 'img/better_sprite.png', 2, 0, 0)
-var ma = new MessageArea(gameInstance.context, 20, 20, 500, 500, 'blue')
+var character = new PlayerCharacter(gameInstance.context, 32, 32, 'img/better_sprite.png', 2, 0, 0, true);
+var ma = new MessageArea(gameInstance.context, 20, 20, 500, 500, false, 'blue');
+var barrier = new Obstacle(gameInstance.context, 20, 250, 300, 200);
+var barrier2 = new Obstacle(gameInstance.context, 200, 20, 250, 350);
+
+var barrier3 = new Obstacle(gameInstance.context, 20, 300, 40, 50);
+var isBarrierSolid = true;
+
+//here we go with this same idea for collisions, drawing, and keyboard input polling
+inputHandler.addPoller(character);
+inputHandler.addPoller(ma);
 
 //whoever is added second gets their draw method called second and is therefore drawn on top
 gameInstance.addDrawable(ma);
+gameInstance.addDrawable(barrier);
+gameInstance.addDrawable(barrier2);
 gameInstance.addDrawable(character);
+gameInstance.addDrawable(barrier3);
+//order won't matter too much here, whoever is added second gets their collision method
+//called second
+collisionResolver.addCollidable(ma);
+collisionResolver.addCollidable(character);
+collisionResolver.addCollidable(barrier);
+collisionResolver.addCollidable(barrier2);
+collisionResolver.addCollidable(barrier3);
 //now that draw is defined we can start the game
 gameInstance.start();
---
+++ js/drawable_classes.js
@@ -0,0 +1,324 @@
+//base drawable class, anything drawn on the screen inherits this.
+class Drawable {
+  constructor(context, width, height, x, y) {
+    this.context = context;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+  }
+  //draw method to overload
+  draw() { }
+}
+
+//
+class Collidable extends Drawable{
+  constructor(context, width, height, x, y, solid) {
+    console.log("is solid: " + solid)
+    super(context, width, height, x, y);
+    this.solid = solid;
+    this.contactList = [];
+  }
+
+  //
+  baseOnContactLost(lostWith, i) {
+    this.contactList.splice(i, 1);
+  }
+
+  //
+  onContactLost(lostWith, i) {
+    this.baseOnContactLost(lostWith, i);
+  }
+
+  //
+  baseOnCollision(collidedWith) {
+    //if it doesn't exist in the contactList, add it
+    if(this.contactList.indexOf(collidedWith) <= -1)
+      this.contactList.push(collidedWith);
+  }
+
+  //
+  onCollison(collidedWith) {
+    this.baseOnCollision(collidedWith);
+  }
+}
+
+//
+class Obstacle extends Collidable {
+  constructor(context, width, height, x, y) {
+    super(context, width, height, x, y, true) //true b/c solid
+  }
+  draw() {
+    this.context.fillStyle = 'black';
+    this.context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  onCollision() {
+    this.baseOnCollision();
+  }
+
+  onContactLost() {
+    this.baseOnContactLost();
+  }
+}
+
+//
+class MessageArea extends Collidable {
+  constructor(context, width, height, x, y, solid, color) {
+    super(context, width, height, x, y, solid);
+    //can't access this in constructor until we call super
+    this.color = color; //color is a string (think css)
+    this.displayMessage = true;
+  }
+
+  //
+  draw() {
+    this.context.fillStyle = this.color;
+    this.context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  //
+  onCollision(collidedwith) {
+    this.baseOnCollision(collidedwith);
+    if(this.displayMessage) {
+      this.color = 'red';
+      this.showMessage();
+      this.displayMessage = false;
+    }
+  }
+
+  //
+  onContactLost(lostWith, i) {
+    this.baseOnContactLost(lostWith, i);
+    //do whatever else you need to do
+    this.displayMessage = true;
+    this.color = 'blue'
+  }
+
+  //
+  pollForKeyboardInput() {
+    if(KEYS[13]) {
+      this.hideMessage();
+    }
+  }
+
+  //
+  showMessage() {
+    var messageArea = document.getElementById('message-area');
+    messageArea.innerHTML = 'Test Message: Press enter to dismiss.';
+  }
+
+  //
+  hideMessage() {
+    var messageArea = document.getElementById('message-area');
+    messageArea.innerHTML = '';
+  }
+}
+
+//any character that moves extends this class
+class Character extends Collidable {
+  constructor(context, width, height, image, speed, x, y, solid) {
+    //super calls the base class constructor
+    //super.methodname() calls that method from the base class
+    super(context, width, height, x, y, solid)
+    this.image =  new Image();
+    this.image.src = image;
+    this.cutX = 0;
+    this.cutY = 0;
+    this.direction = DIRECTIONS.RIGHT;
+    this.animate = false;
+    this.speed = speed;
+    this.dialogue = "";
+    this.isSpeaking = false;
+    this.speechCounter = 0;
+    this.speechDuration;
+  }
+
+  //draws to canvas context based on the source image and the position
+  draw() {
+    charX = this.x;
+    charY = this.y;
+    this.context.drawImage(this.image, (this.cutX * this.width),
+                          (this.height * this.cutY), this.width, this.height,
+                          this.x, this.y,
+                          this.width, this.height);
+    if(this.isSpeaking) {
+      if(this.speechCounter <= this.speechDuration) {
+        this.context.font = "12px Arial";
+        this.context.fillStyle = 'slateblue';
+        this.context.fillText(this.dialogue, this.x - 20, this.y - 20);
+        this.speechCounter += 1;
+      }
+      else {
+        this.silence();
+      }
+    }
+  }
+
+  speak(dialogue, duration) {
+    this.isSpeaking = true;
+    this.dialogue = dialogue;
+    this.speechDuration = duration;
+  }
+
+  silence() {
+    this.isSpeaking = false;
+    this.speechCounter = 0;
+  }
+
+  //move must be overridden if we want to do anything
+  move(up, left, right, down, direction) {}
+}
+
+
+//player character that responds to key input
+class PlayerCharacter extends Character {
+  constructor(context, width, height, image, speed, x, y, solid) {
+    super(context, width, height, image, speed, x, y, solid);
+    this.count = 0;
+    this.canMoveUp = true;
+    this.canMoveLeft = true;
+    this.canMoveRight = true;
+    this.canMoveDown = true;
+  }
+
+  allowMovement() {
+    this.canMoveUp = true;
+    this.canMoveLeft = true;
+    this.canMoveRight = true;
+    this.canMoveDown = true;
+  }
+
+  //
+  onCollision(collidedWith) {
+
+    this.baseOnCollision(collidedWith);
+    //for debug purposes
+    debugContactList = this.contactList;
+    if(collidedWith.solid == true) {
+      if(this.direction == DIRECTIONS.UP) {
+        this.canMoveUp = false;
+        this.bounce(0, 0, 0, 1);
+      }
+      if(this.direction == DIRECTIONS.LEFT) {
+        this.canMoveLeft = false;
+        this.bounce(0, 0, 1, 0);
+      }
+      if(this.direction == DIRECTIONS.RIGHT) {
+        this.canMoveRight = false;
+        this.bounce(0, 1, 0, 0);
+      }
+      if(this.direction == DIRECTIONS.DOWN) {
+        this.canMoveDown = false;
+        this.bounce(1, 0, 0, 0);
+      }
+      this.speak("I can't go that way", 60);
+    }
+    if(collidedWith.constructor.name == 'MessageArea') {
+      this.speak("What is this?", Number.MAX_SAFE_INTEGER); //has another dialogue close condition
+      isBarrierSolid = false;
+    }
+  }
+
+  //
+  onContactLost(lostWith, i) {
+    this.baseOnContactLost(lostWith, i);
+    //for debug purposes
+    debugContactList = this.contactList;
+    if(lostWith.solid)
+      this.allowMovement();
+    if(lostWith.constructor.name == "MessageArea" && this.isSpeaking)
+      this.isSpeaking = false;
+  }
+
+  //checks if keys are pressed and if they are, move and animate
+  // uses the WASD keys
+  // move commands go in as 1 or 0
+  //and animate in the direction of direction
+  move(up, left, right, down, direction) {
+      this.animate = true;
+      if(this.canMoveLeft) this.x -= (left * this.speed);
+      if(this.canMoveRight) this.x += (right * this.speed);
+      if(this.canMoveDown) this.y += (down * this.speed);
+      if(this.canMoveUp) this.y -= (up * this.speed);
+      this.direction = direction;
+      this.setAnimationFrame();
+      //at some point have to turn off animation
+  }
+
+  //an imperceptable move that clears collisions and doesnt check flags
+  bounce(up, left, right, down) {
+    this.x -= (left * 2);
+    this.x += (right * 2);
+    this.y += (down * 2);
+    this.y -= (up * 2);
+  }
+
+  //figure out where on the spritesheet to animate from
+  setAnimationFrame() {
+        if(!this.animate)
+          return;
+        //set yImage based on direction for most sprites
+        this.cutY = this.direction;
+        if(this.count % 15 == 0) { // Every 15 movement frames (frame group) cycle through sprites
+          if (this.count == 0){ // Begin cycle at sprite 2
+            this.cutX = 0;
+          }
+          else if (this.count == 15){ // Go to next sprite for 3 frame groups
+            this.cutX = 1;
+          }
+          else if (this.count == 30){ // Go to previous sprite for 2 frame groups
+            this.cutX = 2;
+            this.count = -1;
+          }
+        }
+        this.count++;
+
+    }
+      //
+  pollForKeyboardInput() {
+      // if (KEYS[65] && KEYS[83]) // Go Left and Down
+      // {
+      // }
+      // else if (KEYS[65] && KEYS[87]) // Go Left and Up
+      // {
+      //   this.move(1, 1, 0, 0, DIRECTIONS.LEFT);
+      // }
+      // else if (KEYS[68] && KEYS[83]) // Go Right and Down
+      // {
+      //   this.move(0, 0, 1, 1, DIRECTIONS.RIGHT);
+      // }
+      // else if (KEYS[68] && KEYS[87]) // Go Right and Up
+      // {
+      //   this.move(1, 0, 1, 0, DIRECTIONS.RIGHT);
+      // }
+   		/*else*/ if (KEYS[65]) // Go Left
+      {
+        this.move(0, 1, 0, 0, DIRECTIONS.LEFT)
+      }
+ 		  else if (KEYS[68]) // Go Right 39
+      {
+        this.move(0, 0, 1, 0, DIRECTIONS.RIGHT)
+      }
+      else if (KEYS[87]) // Go Up
+      {
+        this.move(1, 0, 0, 0, DIRECTIONS.UP);
+      }
+      else if (KEYS[83]) // Go Down
+      {
+        this.move(0, 0, 0, 1, DIRECTIONS.DOWN);
+      }
+      else {
+        this.animate = false;
+      }
+    }
+}
+
+//an 'enum' for directions not sure how much milage we will get out
+//of this as it's pretty specific to our only sprite sheet
+var DIRECTIONS = {
+  UP : 3,
+  LEFT : 1,
+  RIGHT : 2,
+  DOWN : 0
+}
