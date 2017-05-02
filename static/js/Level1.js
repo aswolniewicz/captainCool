b042b80d4775b9bb4efa80a7d8892354acbfdb7d
Serenaise <serenaise@gmail.com>
2017-05-02 11:39:56 -0600

    commiting so that I can pull.

--- static/js/Level1.js
+++ static/js/Level1.js
@@ -9,7 +9,7 @@ var Screen1_4 = new Screen(Level1,'../static/img/background.png','image');
 var Door1_1to1_2 = new Door(Screen1_1, 10, 150, 950, 250,Screen1_2, 'teal',['location',15,300,'keyed','Key_For_Screen2_Door']);
 var Door1_2to1_1 = new Door(Screen1_2, 10, 150, 0, 250,Screen1_1, 'teal',['location',900,300]);
 var Door1_2to1_3 = new Door(Screen1_2, 10, 150, 950, 250,Screen1_3, 'teal',['location',475,500]);
-var Door1_3to1_2 = new Door(Screen1_3, 150, 10, 400, 630,Screen1_2, 'teal',['location',15,300]);
+var Door1_3to1_2 = new Door(Screen1_3, 150, 10, 400, 630,Screen1_2, 'teal',['location',515,300]);
 var Door1_3to1_4 = new Door(Screen1_3, 150, 10, 400, 0,Screen1_4, 'teal',['location',15,300]);
 
 //Level transition door
@@ -36,4 +36,3 @@ var Wall1_3_2 = new Obstacle(gameInstance, 400, 640, 0, 0);
 Screen1_3.addDrawable(Wall1_3_2);
 // Start screen
 Level1.currentScreen=Screen1_1;
-
